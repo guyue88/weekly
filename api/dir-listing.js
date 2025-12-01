c6f887dccfb5fd3af0e817b8d9e25334361abf43
Kian <luoqiang@workmagic.io>
2025-12-01 11:01:51 +0800

    feat: up

--- api/dir-listing.js
+++ api/dir-listing.js
@@ -3,26 +3,27 @@ import path from "path";
 
 export default async function handler(req, res) {
   const reqPath = req.query.path || "";
-  const fullPath = path.join(process.cwd(), reqPath);
+  const safePath = reqPath.replace(/\/$/, ""); // 防止末尾/
+  const fullPath = path.join(process.cwd(), safePath);
 
   try {
     const stat = await fs.stat(fullPath);
 
     if (stat.isDirectory()) {
       const files = await fs.readdir(fullPath);
+
       return res.send(`
-        <h1>Index of /${reqPath}</h1>
+        <h1>Index of /${safePath}</h1>
         <ul>
         ${files
-          .map(f => `<li><a href="/${reqPath}/${f}">${f}</a></li>`)
+          .map(f => `<li><a href="/${safePath}/${f}">${f}</a></li>`)
           .join("")}
         </ul>
       `);
     } else {
-      // serve file normally
       return res.sendFile(fullPath);
     }
-  } catch (e) {
-    res.status(404).send("Not found");
+  } catch {
+    return res.status(404).send("Not found");
   }
 }
